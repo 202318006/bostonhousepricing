e2507556a9aa21f77d65f63f4cfd925cc5af7665
Kushal Barot <202318006@daiict.ac.in>
2024-05-23 18:22:46 +0530

    the web app is ready

--- app.py
+++ app.py
@@ -62,5 +62,13 @@ def predict_api():
         print("Error:", str(e))
         return jsonify({'error': str(e)})
 
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_input=scalar.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output=regmodel.predict(final_input)[0]
+    return render_template("home.html",prediction_text="The House price prediction is {}".format(output))
+
 if __name__ == "__main__":
     app.run(debug=True)
