4197c28d713291bfce5d6b6b12775e1a53d647a5
Kushal Barot <202318006@daiict.ac.in>
2024-05-23 12:01:52 +0530

    this is my second commit

---
+++ app.py
@@ -0,0 +1,66 @@
+# import pickle
+# # from flask import Flask, request, 
+# from flask import Flask,request,app,jsonify,url_for,render_template
+# import numpy as np
+# import pandas as pd
+
+# app = Flask(__name__)
+# regmodel = pickle.load(open('regmodel.pkl','rb'))
+# scalar = pickle.load(open('scaling.pkl','rb'))
+
+
+# @app.route('/')
+# def home():
+#     return render_template('home.html')
+
+# @app.route('/predict_api',methods=['POST'])
+
+# def predict_api():
+#     data = request.json['data']
+#     print(data)
+#     print(np.array(list(data.values())).reshape(1,-1))
+#     new_data = scalar.transform(np.array(list(data.values())).reshape(1,-1))
+#     output = regmodel.predict(new_data)[0]
+#     print(output[0])
+#     return jsonify(output)
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
+import pickle
+from flask import Flask, request, jsonify, render_template
+import numpy as np
+import pandas as pd
+
+app = Flask(__name__)
+regmodel = pickle.load(open('regmodel.pkl', 'rb'))
+scalar = pickle.load(open('scaling.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/predict_api', methods=['POST'])
+def predict_api():
+    data = request.json['data']
+    try:
+        # Convert the input data to a numpy array and reshape
+        new_data = np.array(list(data.values())).reshape(1, -1)
+        print("New data:", new_data)
+
+        # Scale the data
+        new_data_scaled = scalar.transform(new_data)
+        print("Scaled data:", new_data_scaled)
+
+        # Make prediction
+        output = regmodel.predict(new_data_scaled)
+        print("Prediction output:", output)
+
+        # Return the prediction as a JSON response
+        return jsonify({'prediction': output[0]})
+    except Exception as e:
+        # Print the error and return it in the response
+        print("Error:", str(e))
+        return jsonify({'error': str(e)})
+
+if __name__ == "__main__":
+    app.run(debug=True)
